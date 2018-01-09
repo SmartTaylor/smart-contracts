15ae4400e2ce51ad9fe5318a7567a49fb153aff7
JG Carvalho <jgcarvalho93@gmail.com>
2018-01-09 11:01:08 -0200

    improved migration flow

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,26 +5,37 @@ var TokenVesting = artifacts.require('./TokenVesting.sol');
 const latestTime = require('../test/helpers/latestTime.js');
 const duration = require('../test/helpers/increaseTime.js').duration;
 
+
+let getNow = new Promise((resolve, reject) => {
+  web3.eth.getBlock('latest', (err, block) => {
+    err ? reject(err) : resolve(block);
+  })
+})
+
 module.exports = function (deployer, network, accounts) {
 
-  // console.log('Network', network);
-  // console.log('Accounts', accounts);
-  //
-  // let owner = accounts[0];
-  // let tokenInstance, saleInstance;
-  // const wallet = accounts[1];
-  //
-  // deployer.then(function(){
-  //   return TaylorToken.new({from: owner})
-  // }).then(function (tkn){
-  //   tokenInstance = tkn;
-  //   console.log("Before instantiating sale");
-  //     return Crowdsale.new(1915197936, 2, 6535 * Math.pow(10,21), tokenInstance.address, wallet, {from:owner, gas:1500000});
-  //   })
-  //   .then(function(crwd){
-  //   icoAddress = crwd.address;
-  //   console.log("Token address: ", tokenInstance.address);
-  //   console.log("Sale address: ", icoAddress);
-  //   return tokenInstance.transfer(icoAddress, 6535 * Math.pow(10,21), { from: owner})
-  // })
+  console.log('Network', network);
+  console.log('Accounts', accounts);
+
+  let owner = accounts[0];
+  let tokenInstance, saleInstance;
+  const wallet = accounts[1];
+  let now = 0;
+
+    getNow.then((block) => {
+      now = block.timestamp;
+      return deployer
+    }).then(function(){
+      return TaylorToken.new({from: owner})
+    }).then(function (tkn){
+      tokenInstance = tkn;
+      console.log("Before instantiating sale");
+        return Crowdsale.new(now + 300, 2, 6535 * Math.pow(10,21), tokenInstance.address, wallet, {from:owner, gas:1500000});
+      })
+      .then(function(crwd){
+      icoAddress = crwd.address;
+      console.log("Token address: ", tokenInstance.address);
+      console.log("Sale address: ", icoAddress);
+      return tokenInstance.transfer(icoAddress, 6535 * Math.pow(10,21), { from: owner})
+    })
 };
